b1e65e329570d6f25153973e2b6b503f8e72e62a
edire <az8641683@163.com>
2014-01-29 17:19:41 +0800

    aaa

--- report.php
+++ report.php
@@ -1,7 +1,19 @@
 <?php
 if(!empty($_POST['xh'])){
 	$xh=$_POST['xh'];
+
 	
+$url="http://218.56.38.235:8000/auth/logout.do";
+$post="username=zsr&password=rn2012"
+$cookie_file=tempnam('./tmp','cookie');
+$ch = curl_init($url) ;
+curl_setopt($ch,CURLOPT_HEADER,0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+curl_setopt($ch, CURLOPT_POST,1) ; 
+curl_setopt($ch,CURLOPT_COOKIEJAR,$cookie_file);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$post); 
+curl_exec($ch);
+curl_close($ch);
 	
 
 
@@ -11,6 +23,7 @@ curl_setopt($ch,CURLOPT_HEADER,0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0) ;
 curl_setopt($ch, CURLOPT_POST,1) ; 
 curl_setopt($ch, CURLOPT_POSTFIELDS,$post); 
+		curl_setopt($ch,CURLOPT_COOKIEFILE,$cookie_file);
 $data=curl_exec($ch);
 curl_close($ch);
 
@@ -31,6 +44,7 @@ curl_setopt($ch,CURLOPT_HEADER,0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
 curl_setopt($ch, CURLOPT_POST,1) ; 
 curl_setopt($ch, CURLOPT_POSTFIELDS,$post); 
+		curl_setopt($ch,CURLOPT_COOKIEFILE,$cookie_file);
 curl_exec($ch);
 curl_close($ch);
 
