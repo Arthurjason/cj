b64135d7e022cb386ea93d52953d1888810da46b
edire <az8641683@163.com>
2014-01-29 13:40:15 +0800

    aaa

--- report.php
+++ report.php
@@ -6,7 +6,7 @@ $url='http://202.194.48.11:9004/setReportParams';
 $post="LS_XH=$xh";
 $ch = curl_init($url) ;
 curl_setopt($ch,CURLOPT_HEADER,0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0) ;
 curl_setopt($ch, CURLOPT_POST,1) ; 
 curl_setopt($ch, CURLOPT_POSTFIELDS,$post); 
 curl_exec($ch);
