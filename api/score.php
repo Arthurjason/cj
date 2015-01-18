84f62a4543e24c45447f483a264693d85609e1f3
root <root@localhost.localdomain>
2015-01-18 20:28:55 +0800

    update

--- api/score.php
+++ api/score.php
@@ -1,6 +1,10 @@
 <?php
-	require('phpQuery/phpQuery.php');
 
-	$doc = phpQuery::newDocument('<div/>');
+	include('phpQuery/phpQuery.php');
 
+//	$data = file_get_contents('data.txt');
+//	print_r($data);
+	phpQuery::newDocumentHTML($data);
 	
+	echo pq('body');
+
